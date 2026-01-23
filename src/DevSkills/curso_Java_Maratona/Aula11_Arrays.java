d7cf425c61cb521f31d64c0fc99ead208979ed97
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-01-23 17:57:34 -0300

    Create Aula11_Arrays.java

---
+++ src/DevSkills/curso_Java_Maratona/Aula11_Arrays.java
@@ -0,0 +1,18 @@
+package DevSkills.curso_Java_Maratona;
+
+public class Aula11_Arrays {
+    static void main() {
+        int[] idades = new int[5];
+        idades[0] = 23;
+        idades[1] = 20;
+        idades[2] = 15;
+        idades[3] = 78;
+        idades[4] = 93;
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+        System.out.println(idades[3]);
+        System.out.println(idades[4]);
+
+    }
+}
