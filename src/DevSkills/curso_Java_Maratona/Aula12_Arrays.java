5c179c0a067c9732ec68df7b6604e117f646e839
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-01-24 21:35:57 -0300

    Create Aula12_Arrays.java

---
+++ src/DevSkills/curso_Java_Maratona/Aula12_Arrays.java
@@ -0,0 +1,16 @@
+package DevSkills.curso_Java_Maratona;
+
+public class Aula12_Arrays {
+    static void main() {
+        // byte , short, int, long, float, double
+        // char
+        // boolean
+        // String
+
+        String[] teste = new String[3];
+
+        System.out.println(teste[0]);
+        System.out.println(teste[1]);
+        System.out.println(teste[2]);
+    }
+}
