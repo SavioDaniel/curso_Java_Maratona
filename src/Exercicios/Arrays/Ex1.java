63c7eab15c610abec77633ed67e5b4312b715a51
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-01-25 15:00:07 -0300

    Create Ex1.java

---
+++ src/Exercicios/Arrays/Ex1.java
@@ -0,0 +1,10 @@
+package Exercicios.Arrays;
+
+public class Ex1 {
+    static void main() {
+        int [] numb = new int[]{2, 4, 6, 8, 10};
+        for (int array: numb){
+            System.out.println(array);
+        }
+    }
+}
