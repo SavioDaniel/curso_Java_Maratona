398433b61430e8d2e8e9c98d743e8ab6911c46d4
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-01-25 15:00:09 -0300

    Create Ex2.java

---
+++ src/Exercicios/Arrays/Ex2.java
@@ -0,0 +1,13 @@
+package Exercicios.Arrays;
+
+public class Ex2 {
+    static void main() {
+        int[] valores = {5, 3, 7, 2};
+        int teste = 0;
+        for (int array: valores){
+            teste +=array ;
+        }
+        System.out.println(teste);
+
+    }
+}
