51ec0525ac1ac027c277e55acef5afeeaf062a9f
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-01-25 23:37:40 -0300

    Create CadastroDePessoas.java

---
+++ src/Exercicios/Exercicios_Complexos/CadastroDePessoas.java
@@ -0,0 +1,19 @@
+package Exercicios.Exercicios_Complexos;
+import java.util.Scanner;
+public class CadastroDePessoas {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite sua idade: ");
+        int idade = sc.nextInt();
+
+        System.out.print("Digite seu nome: ");
+        String nome = sc.next();
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+
+        sc.close();
+    }
+}
