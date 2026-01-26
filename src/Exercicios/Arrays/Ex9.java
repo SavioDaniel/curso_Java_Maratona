8a523e5cdd9c8805d5559a10bde11776d99d03a0
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-01-25 23:37:38 -0300

    Create Ex9.java

---
+++ src/Exercicios/Arrays/Ex9.java
@@ -0,0 +1,8 @@
+package Exercicios.Arrays;
+//3) Escreva um programa para ler um vetor C de N posi¢des, onde N é menor ou igual a 20 e
+// imprimir seus elementos. O usuario deve informar o nimero de posicdes e os elementos.
+public class Ex9 {
+    static void main() {
+
+    }
+}
