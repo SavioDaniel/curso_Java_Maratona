710e88a01f0e73dc6c7c175e554ba20344a38f52
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-01-28 19:59:00 -0300

    Create CalculadoraTest2.java

---
+++ src/introducaoMetodos/Test/CalculadoraTest2.java
@@ -0,0 +1,11 @@
+package introducaoMetodos.Test;
+
+import introducaoMetodos.Dominio.Calculadora;
+
+public class CalculadoraTest2 {
+    static void main() {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumeros(10, 20);
+
+    }
+}
