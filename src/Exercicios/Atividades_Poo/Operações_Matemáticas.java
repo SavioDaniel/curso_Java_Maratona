8f67e78e2fe4b96f59eac11139f0450f86d8de68
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-01-28 19:58:43 -0300

    Create Operações_Matemáticas.java

---
+++ src/Exercicios/Atividades_Poo/Operações_Matemáticas.java
@@ -0,0 +1,16 @@
+package Exercicios.Atividades_Poo;
+
+public class Operações_Matemáticas {
+    static void main() {
+        //criando as duas variáveis para receber os valores 10 e 5.
+        int numero1 = 10;
+        int numero2 = 5;
+
+        //Logica e print
+        System.out.printf("A soma entre %d + %d = %d", numero1, numero2, (numero1 + numero2));
+        System.out.printf("\nA soma entre %d - %d = %d", numero1, numero2, (numero1 - numero2));
+        System.out.printf("\nA soma entre %d * %d = %d", numero1, numero2, (numero1 * numero2));
+        System.out.printf("\nA soma entre %d / %d = %d", numero1, numero2, (numero1 / numero2));
+
+    }
+}
