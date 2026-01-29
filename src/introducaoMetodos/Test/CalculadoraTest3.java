377ebbf8abfc9df6b622d87fb2a218572f069c4d
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-01-29 20:55:32 -0300

    Update CalculadoraTest3.java

--- src/introducaoMetodos/Test/CalculadoraTest3.java
+++ src/introducaoMetodos/Test/CalculadoraTest3.java
@@ -5,7 +5,9 @@ import introducaoMetodos.Dominio.Calculadora;
 public class CalculadoraTest3 {
     static void main() {
         Calculadora calculadora = new Calculadora();
-        double resultado = calculadora.divideDoisNumeros(10, 0);
-        System.out.println(resultado);
+       // double resultado = calculadora.divideDoisNumeros(10, 0);
+        //System.out.println(resultado);
+
+        calculadora.divisao(21,0);
     }
 }
