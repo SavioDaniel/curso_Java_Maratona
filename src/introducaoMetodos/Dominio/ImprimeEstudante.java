c89e8f2ae4887ee1a70bbd109b4a09497fda1e03
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-01-29 20:51:37 -0300

    Create ImprimeEstudante.java

---
+++ src/introducaoMetodos/Dominio/ImprimeEstudante.java
@@ -0,0 +1,13 @@
+package introducaoMetodos.Dominio;
+
+public class ImprimeEstudante {
+    public void Imprime(Estudante estudante){
+
+        System.out.println("---------------------");
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+
+
+    }
+}
