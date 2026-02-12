534aa5cdb912ec62467887b7c815663f83bb7397
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-11 23:26:26 -0300

    Update ImprimeAluno.java

--- src/introducaoPrivate/dominio/ImprimeAluno.java
+++ src/introducaoPrivate/dominio/ImprimeAluno.java
@@ -2,6 +2,8 @@ package introducaoPrivate.dominio;
 
 public class ImprimeAluno {
     public void print(Aluno aluno){
-        System.out.println(aluno);
+        System.out.println(aluno.getNome());
+        System.out.println(aluno.getIdade());
+        System.out.println(aluno.getSexo());
     }
 }
