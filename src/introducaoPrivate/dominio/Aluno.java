60836f5126efe047f74d795ae76e48ece62a39a8
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-11 23:26:24 -0300

    Update Aluno.java

--- src/introducaoPrivate/dominio/Aluno.java
+++ src/introducaoPrivate/dominio/Aluno.java
@@ -11,5 +11,27 @@ public class Aluno {
         System.out.println(this.sexo);
     }
 
+    public void setNome(String nome){
+        this.nome = nome;
+    }
 
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
 }
