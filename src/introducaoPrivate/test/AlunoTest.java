c96b2ab80227790c62df86ba3a51b6cd12688ec7
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-11 23:26:29 -0300

    Update AlunoTest.java

--- src/introducaoPrivate/test/AlunoTest.java
+++ src/introducaoPrivate/test/AlunoTest.java
@@ -1,4 +1,17 @@
 package introducaoPrivate.test;
 
+import introducaoPrivate.dominio.Aluno;
+import introducaoPrivate.dominio.ImprimeAluno;
+
 public class AlunoTest {
+    static void main() {
+        Aluno aluno = new Aluno();
+        ImprimeAluno imprimeAluno = new ImprimeAluno();
+        aluno.setNome("Sávio");
+
+        imprimeAluno.print(aluno);
+
+
+
+    }
 }
