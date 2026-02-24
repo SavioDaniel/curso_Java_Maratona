76c40852d89b55300f24f55642d2eff78376e255
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-24 20:48:44 -0300

    Delete ImprimeAluno.java

--- src/introducaoPrivate/dominio/ImprimeAluno.java
+++
@@ -1,9 +0,0 @@
-package introducaoPrivate.dominio;
-
-public class ImprimeAluno {
-    public void print(Aluno aluno){
-        System.out.println(aluno.getNome());
-        System.out.println(aluno.getIdade());
-        System.out.println(aluno.getSexo());
-    }
-}
