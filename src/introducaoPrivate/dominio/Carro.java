9b6f4bd444c8c4efa0dfe47827e14ed481417a5a
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-24 21:43:45 -0300

    Update Carro.java

--- src/introducaoPrivate/dominio/Carro.java
+++ src/introducaoPrivate/dominio/Carro.java
@@ -5,13 +5,35 @@ public class Carro {
     private int ano;
     private double valor;
 
+
+    //Imprime os valores
+    public  void Imprime(){
+        System.out.println(this.modelo);
+        System.out.println(this.ano);
+        System.out.println(this.valor);
+    }
+
     public void setModelo(String modelo){
         this.modelo = modelo;
     }
 
-    
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
 
     public String getModelo() {
         return modelo;
+    }
+
+    public double getValor() {
+       return valor;
+    }
+
+    public int getAno() {
+        return ano;
     }
 }
