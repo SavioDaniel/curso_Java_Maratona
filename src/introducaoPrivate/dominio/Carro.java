534b91b792833c5f8985a09d18001fe3d8c21e81
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-24 22:55:38 -0300

    Update Carro.java

--- src/introducaoPrivate/dominio/Carro.java
+++ src/introducaoPrivate/dominio/Carro.java
@@ -1,5 +1,7 @@
 package introducaoPrivate.dominio;
 
+import java.util.Locale;
+
 public class Carro {
     private String modelo;
     private int ano;
@@ -14,15 +16,23 @@ public class Carro {
     }
 
     public void setModelo(String modelo){
-        this.modelo = modelo;
+        this.modelo = modelo.toUpperCase(Locale.ROOT);
     }
 
     public void setValor(double valor) {
-        this.valor = valor;
+        if (valor > 0){
+            this.valor = valor;
+        }else {
+            System.out.println("Valor errado");
+        }
     }
 
     public void setAno(int ano) {
-        this.ano = ano;
+        if (ano > 0) {
+            this.ano = ano;
+        }else {
+            System.out.println("Ano errado");
+        }
     }
 
     public String getModelo() {
