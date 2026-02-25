32be8c87d556c8e45aa13cc1caa175b2cf4a1807
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-24 21:24:00 -0300

    Update CarroTest.java

--- src/introducaoPrivate/test/CarroTest.java
+++ src/introducaoPrivate/test/CarroTest.java
@@ -1,4 +1,12 @@
 package introducaoPrivate.test;
 
+import introducaoPrivate.dominio.Carro;
+
 public class CarroTest {
+    static void main(String[] args) {
+        Carro carro = new Carro();
+
+        carro.setModelo("Camaro");   // define
+        System.out.println(carro.getModelo());
+    }
 }
