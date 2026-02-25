09df31586d335db42dee08145609e46222613267
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-24 21:23:58 -0300

    Update Carro.java

--- src/introducaoPrivate/dominio/Carro.java
+++ src/introducaoPrivate/dominio/Carro.java
@@ -5,25 +5,11 @@ public class Carro {
     private int ano;
     private double valor;
 
-    public void setAno(int ano) {
-        this.ano = ano;
-    }
-
-    public void setModelo(String modelo) {
+    public void setModelo(String modelo){
         this.modelo = modelo;
     }
 
-    public void setValor(double valor) {
-        this.valor = valor;
-    }
-
-    public int getAno() {
-        return ano;
-    }
-
-    public double getValor() {
-        return valor;
-    }
+    
 
     public String getModelo() {
         return modelo;
