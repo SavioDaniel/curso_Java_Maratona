380a5c1c7f836d2d521c9ef103a06e47d8e8808c
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-24 21:43:48 -0300

    Update CarroTest.java

--- src/introducaoPrivate/test/CarroTest.java
+++ src/introducaoPrivate/test/CarroTest.java
@@ -1,12 +1,26 @@
 package introducaoPrivate.test;
-
+import java.util.Scanner;
 import introducaoPrivate.dominio.Carro;
+import introducaoPrivate.dominio.Imprime;
 
 public class CarroTest {
     static void main(String[] args) {
+        //Bibliotecas
         Carro carro = new Carro();
+        Scanner scanner = new Scanner(System.in);
 
-        carro.setModelo("Camaro");   // define
+        int ano1 = scanner.nextInt();
+        int valor1 = sca
+        //função set
+        carro.setAno(ano1);
+        carro.setValor(86.567);
+        carro.setModelo("Camaro");
+
+        //função que usa o get.
+
         System.out.println(carro.getModelo());
+        System.out.println(carro.getAno());
+        System.out.println(carro.getValor());
+
     }
 }
