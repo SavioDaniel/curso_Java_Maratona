672a91957002a5a3ea4bd757c5ca6b1412750467
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-24 22:55:43 -0300

    Update CarroTest.java

--- src/introducaoPrivate/test/CarroTest.java
+++ src/introducaoPrivate/test/CarroTest.java
@@ -1,31 +1,35 @@
 package introducaoPrivate.test;
-
 import java.util.Scanner;
 import introducaoPrivate.dominio.Carro;
+import introducaoPrivate.dominio.Imprime;
 
 public class CarroTest {
-    public static void main(String[] args) {
-
+    static void main(String[] args) {
+        //Bibliotecas
         Carro carro = new Carro();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite o ano: ");
-        int ano1 = scanner.nextInt();
-
-        System.out.print("Digite o valor: ");
-        double valor1 = scanner.nextDouble();
-        scanner.nextLine(); // limpa ENTER
-
-        System.out.print("Digite o modelo: ");
+        // parte que eu uso o scanner para da valores
+        System.out.println("Qual o modelo do carro: ");
         String modelo1 = scanner.nextLine();
 
+        System.out.println("Ano do carro: ");
+        int ano1 = scanner.nextInt();
+
+        System.out.println("Valor do carro: ");
+        double valor1 = scanner.nextDouble();
+
+
+        //função set
+        carro.setModelo(modelo1);
         carro.setAno(ano1);
         carro.setValor(valor1);
-        carro.setModelo(modelo1);
 
-        System.out.println("\n--- Dados do Carro ---");
+        //função que usa o get.
+        System.out.println("______________________");
         System.out.println(carro.getModelo());
         System.out.println(carro.getAno());
         System.out.println(carro.getValor());
+
     }
 }
