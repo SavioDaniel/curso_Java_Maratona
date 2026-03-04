bd61a0cd06603fec1d22da1b930870667ecd2ee9
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-03-04 15:56:22 -0300

    Create CarroTest.java

---
+++ src/introducaoStatic/test/CarroTest.java
@@ -0,0 +1,13 @@
+package introducaoStatic.test;
+
+import introducaoStatic.dominio.Carro;
+
+public class CarroTest {
+    static void main(String[] args) {
+        Carro carro1 = new Carro();
+        Carro carro2 = new Carro();
+        Carro carro3 = new Carro();
+
+
+    }
+}
