6f04d14de008cba37396fabe6ea6e7cc6e27f152
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-03-09 20:19:20 -0300

    Create SistemaTest.java

---
+++ src/introducaoStatic/test/SistemaTest.java
@@ -0,0 +1,12 @@
+package introducaoStatic.test;
+
+import introducaoStatic.dominio.Sistema;
+
+public class SistemaTest {
+    static void main(String[] args) {
+        System.out.println("Sistema: " + Sistema.nomeSistema);
+        System.out.println("Máximo de usuários: " + Sistema.maxUsuarios);
+
+
+    }
+}
