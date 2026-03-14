96793d9248d660cfbecc2b24e51f00c8d6e95a4e
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-03-13 22:05:56 -0300

    Update Jogador.java

--- src/introducaoGassociacao/dominio/Jogador.java
+++ src/introducaoGassociacao/dominio/Jogador.java
@@ -2,6 +2,14 @@ package introducaoGassociacao.dominio;
 
 public class Jogador {
     private String nome;
+    private Time time;
+
+    public void imprime(){
+        System.out.println(this.nome);
+        if (time != null){
+            System.out.println(this.time.getNome());
+        }
+    }
 
     public Jogador(String nome) {
         this.nome = nome;
@@ -15,12 +23,17 @@ public class Jogador {
         return nome;
     }
 
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    @Override
-    public String toString() {
-        return " nome: " + nome;
-    }
+
 }
