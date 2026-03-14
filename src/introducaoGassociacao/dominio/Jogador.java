bf56c56e7bf0b7ccb86b6641d6893db373701c65
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-03-13 21:47:35 -0300

    Classe Jogadores
    Hj aprendemos como usar Array para 3 objetos.

---
+++ src/introducaoGassociacao/dominio/Jogador.java
@@ -0,0 +1,26 @@
+package introducaoGassociacao.dominio;
+
+public class Jogador {
+    private String nome;
+
+    public Jogador(String nome) {
+        this.nome = nome;
+    }
+
+    public Jogador(){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return " nome: " + nome;
+    }
+}
