3ea8fc2c372a0b5fa544cfc5e64449f6c1c487d1
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-03-13 21:47:42 -0300

    Create Jogadortest.java

---
+++ src/introducaoGassociacao/test/Jogadortest.java
@@ -0,0 +1,17 @@
+package introducaoGassociacao.test;
+
+import introducaoGassociacao.dominio.Jogador;
+
+public class Jogadortest {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Pelé");
+        Jogador jogador2 = new Jogador("Romário");
+        Jogador jogador3 = new Jogador("Gabigol");
+        Jogador[] jogadores = new Jogador[]{jogador1, jogador2,jogador3};
+        
+        for (Jogador jogador : jogadores) {
+            System.out.println(jogador);
+        }
+
+    }
+}
