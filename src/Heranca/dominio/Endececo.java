a6ef855bdffa4648bb06afedd85a1b6c1ebfd639
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-03-22 14:28:26 -0300

    Create Endececo.java

---
+++ src/Heranca/dominio/Endececo.java
@@ -0,0 +1,19 @@
+package Heranca.dominio;
+
+public class Endececo {
+    private String cidade;
+    private String cep;
+
+    public Endececo(String cidade, String cep) {
+        this.cidade = cidade;
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+}
