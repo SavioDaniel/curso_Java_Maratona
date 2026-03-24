25824fb328bb5cfee31c91bc1eb5dab14e068fe7
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-03-24 13:08:40 -0300

    teste
    testando o final

---
+++ src/Jmodificadorfinal/test/CarroTeste.java
@@ -0,0 +1,10 @@
+package Jmodificadorfinal.test;
+
+import Jmodificadorfinal.dominio.Carro;
+
+public class CarroTeste {
+    static void main(String[] args) {
+        Carro carro = new Carro("Palio");
+        System.out.println(carro.Velocidade_limite);
+    }
+}
