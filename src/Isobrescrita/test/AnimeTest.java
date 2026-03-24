fa2456fd1589ab08563435c5f5c44a579a701bae
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-03-24 13:08:01 -0300

    teste
    testando o toString

---
+++ src/Isobrescrita/test/AnimeTest.java
@@ -0,0 +1,11 @@
+package Isobrescrita.test;
+
+import Isobrescrita.dominio.Anime;
+
+public class AnimeTest {
+    static void main(String[] args) {
+        Anime anime = new Anime("Naruto");
+
+        System.out.println(anime);
+    }
+}
