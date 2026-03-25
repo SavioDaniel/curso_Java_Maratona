64359f0361f1fc90628ac241181028f08678d8d1
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-03-25 13:55:21 -0300

    Add Comprador
    imprimindo nome comprador

--- src/Jmodificadorfinal/test/CarroTeste.java
+++ src/Jmodificadorfinal/test/CarroTeste.java
@@ -1,10 +1,14 @@
 package Jmodificadorfinal.test;
 
 import Jmodificadorfinal.dominio.Carro;
+import Jmodificadorfinal.dominio.Comprador;
 
 public class CarroTeste {
     static void main(String[] args) {
         Carro carro = new Carro("Palio");
         System.out.println(carro.Velocidade_limite);
+
+        carro.comprador.setNomeComprador("Claudio");
+        System.out.println(carro.comprador);
     }
 }
