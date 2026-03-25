734f3d9634e285fbe3f5962d0440828e31dc376d
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-03-25 15:32:52 -0300

    Update CarroTeste.java

--- src/Jmodificadorfinal/test/CarroTeste.java
+++ src/Jmodificadorfinal/test/CarroTeste.java
@@ -7,7 +7,6 @@ public class CarroTeste {
     static void main(String[] args) {
         Carro carro = new Carro("Palio");
         System.out.println(carro.Velocidade_limite);
-
         carro.comprador.setNomeComprador("Claudio");
         System.out.println(carro.comprador);
     }
