ae15535e71fa2612b5d2965e4094e87aaf2a5d4e
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-03-25 13:54:25 -0300

    Modificações
    Coloquei gets e sets referentes a nova classe Comprador

--- src/Jmodificadorfinal/dominio/Carro.java
+++ src/Jmodificadorfinal/dominio/Carro.java
@@ -5,10 +5,16 @@ import java.util.StringJoiner;
 public class Carro {
     private String nomeCarro;
     public final double Velocidade_limite;
+    public final Comprador comprador = new Comprador();
 
 
     public Carro(String nomeCarro) {
         Velocidade_limite = 220;
+        this.nomeCarro = nomeCarro;
+    }
+
+    public Carro(double velocidade_limite, String nomeCarro) {
+        Velocidade_limite = velocidade_limite;
         this.nomeCarro = nomeCarro;
     }
 
@@ -20,4 +26,11 @@ public class Carro {
         this.nomeCarro = nomeCarro;
     }
 
+    public double getVelocidade_limite() {
+        return Velocidade_limite;
+    }
+
+    public Comprador getComprador() {
+        return comprador;
+    }
 }
