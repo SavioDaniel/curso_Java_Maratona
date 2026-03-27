6ba6bfa10816eb4d9a2d93721de86b3916494499
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-03-27 14:40:02 -0300

    Modificações
    Crie na classe Pai um metodo e extend ele para os filhos

--- src/Lclassesabstratas/dominio/Gerente.java
+++ src/Lclassesabstratas/dominio/Gerente.java
@@ -7,6 +7,11 @@ public class Gerente extends Funcionario{
     }
 
     @Override
+    public void salarioBonus() {
+        this.salario = salario + salario * 0.05;
+    }
+
+    @Override
     public String toString() {
         return "Gerente" +
                 "\nnome = " + nome +
