760463a233f3dadba693c7cd0b81d65aaf7908d1
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-03-27 15:08:16 -0300

    Modificação

--- src/Lclassesabstratas/dominio/Funcionario.java
+++ src/Lclassesabstratas/dominio/Funcionario.java
@@ -2,7 +2,7 @@ package Lclassesabstratas.dominio;
 
 import java.util.StringJoiner;
 
-public abstract class Funcionario {
+public abstract class Funcionario extends Pessoa{
     protected String nome;
     protected double salario;
 
@@ -10,6 +10,11 @@ public abstract class Funcionario {
         this.nome = nome;
         this.salario = salario;
         salarioBonus();
+    }
+
+    @Override
+    public void imprime() {
+        System.out.println("Imprimindo....");
     }
 
     public abstract void salarioBonus();
