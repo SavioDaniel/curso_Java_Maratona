de758854a2d11e9b656ce9ace6dccc9fb1fe0bcd
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-03-27 14:25:23 -0300

    Classe Gerente
    Classe filha de funcionario - informações dos gerentes

---
+++ src/Lclassesabstratas/dominio/Gerente.java
@@ -0,0 +1,16 @@
+package Lclassesabstratas.dominio;
+
+public class Gerente extends Funcionario{
+    public Gerente(String nome, double salario) {
+        super(nome, salario);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Gerente" +
+                "\nnome = " + nome +
+                "\nsalario = " + salario
+                ;
+    }
+}
