25b93913cec9a65fee7e4efeddac7e9907b77e7e
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-03-30 20:51:35 -0300

    Create Tomate.java

---
+++ src/Npolimorfismo/dominio/Tomate.java
@@ -0,0 +1,14 @@
+package Npolimorfismo.dominio;
+
+public class Tomate extends Produto{
+    public static final double IMPOSTO_POR_CENTO = 0.06;
+    public Tomate(String nome, double valor) {
+        super(nome, valor);
+    }
+
+    @Override
+    public double calcularimposto() {
+        System.out.println("Calculando imposto do Tomate");
+        return this.valor * IMPOSTO_POR_CENTO;
+    }
+}
