18bc1a0a39d7709bce21cfc80a786082b07012f5
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-05-12 20:28:53 -0300

    Exception

--- src/Oexececao/exception/test/Main.java
+++ src/Oexececao/exception/test/Main.java
@@ -1,4 +1,22 @@
-package Oexececao.exception;
+package Oexececao.exception.test;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
+    static void main(String[] args) {
+        criarNovoArquivo();
+    }
+
+    private static  void criarNovoArquivo() {
+        File file = new File("arquivo\\teste.txt");
+        try{
+            boolean isCriado = file.createNewFile();
+            System.out.println("Arquivo criado: " + isCriado);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
+
+
