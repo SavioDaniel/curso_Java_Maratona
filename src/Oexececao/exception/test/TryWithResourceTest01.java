1af6da71280de4aeba7898c7d591b965a567d2e1
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-05-17 23:40:34 -0300

    Create TryWithResourceTest01.java

---
+++ src/Oexececao/exception/test/TryWithResourceTest01.java
@@ -0,0 +1,13 @@
+package Oexececao.exception.test;
+
+public class TryWithResourceTest01 {
+    static void main() {
+
+    }
+
+    public static void ler(){
+        try(){
+            
+        }
+    }
+}
