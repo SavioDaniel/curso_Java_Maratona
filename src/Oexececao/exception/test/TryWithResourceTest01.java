9ad59b89cb81bebeb19f8a6a021876ae3b722e06
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-05-18 00:11:18 -0300

    Update TryWithResourceTest01.java

--- src/Oexececao/exception/test/TryWithResourceTest01.java
+++ src/Oexececao/exception/test/TryWithResourceTest01.java
@@ -1,13 +1,24 @@
 package Oexececao.exception.test;
 
+import Oexececao.exception.test.dominio.Leitor01;
+import Oexececao.exception.test.dominio.Leitor02;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class TryWithResourceTest01 {
     static void main() {
+        lerArquivos();
 
     }
 
-    public static void ler(){
-        try(){
-            
+    public static void lerArquivos() {
+        try (Leitor01 leitor01= new Leitor01();
+             Leitor02 leitor02 = new Leitor02()) {
+
+
+        } catch (Exception e) {
+
         }
     }
 }
