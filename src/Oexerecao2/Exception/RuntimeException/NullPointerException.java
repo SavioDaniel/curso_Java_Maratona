2b6a91cebe7f47de1ee464a09b22d18f26b2b24c
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-05-19 21:48:52 -0300

    Create NullPointerException.java

---
+++ src/Oexerecao2/Exception/RuntimeException/NullPointerException.java
@@ -0,0 +1,8 @@
+package Oexerecao2.Exception.RuntimeException;
+
+public class NullPointerException {
+    static void main(String[] args) {
+        Integer x = null;
+        int y = x + x;
+    }
+}
