debd3c07a754039e40d4c78ad1b424e4e9808bcb
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-05-25 15:24:51 -0300

    Introdução a Classe utilitaria

---
+++ src/Pwrapper/WrapperTest01.java
@@ -0,0 +1,19 @@
+package Pwrapper;
+
+public class WrapperTest01 {
+    static void main(String[] args) {
+        Byte bytew = 10;
+        Short shortw = 20;
+        Integer intw = 30;
+        Long longw = 40L;
+        Float floatw = 3.14f;
+        Double doublew = 3.1415;
+        Character charw = 'A';
+        Boolean booleanw = true;
+
+        Integer int2 = Integer.compare(10, 10);
+        System.out.println(int2);
+
+
+    }
+}
