9b5c64e391842ed2f95e8a713768fc01cc3961e8
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-05-26 19:47:05 -0300

    Aula 110 concluída

--- src/Qstring/StringPerformaceTest.java
+++ src/Qstring/StringPerformaceTest.java
@@ -2,14 +2,43 @@ package Qstring;
 
 public class StringPerformaceTest {
     public static void main(String[] args) {
+        // Teste com String
         long inicio = System.currentTimeMillis();
-        testandoStraing();
+        testandoStraing(1000_000);
+        long fimString = System.currentTimeMillis();
+        System.out.println("Tempo gasto com String: " + (fimString - inicio) + " ms");
+
+        // Teste com StringBuilder
+        inicio = System.currentTimeMillis();
+        testeStringBuilder(1000_000);
+        long fimStringBuilder = System.currentTimeMillis();
+        System.out.println("Tempo gasto com StringBuilder: " + (fimStringBuilder - inicio) + " ms");
+
+        // Teste com StringBuffer
+        inicio = System.currentTimeMillis();
+        testeStringBuffer(1000_000);
+        long fimStringBuffer = System.currentTimeMillis();
+        System.out.println("Tempo gasto com StringBuffer: " + (fimStringBuffer - inicio) + " ms");
     }
 
-    private static void testandoStraing() {
+    private static void testandoStraing(int tamanho) {
         String texto = "";
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < tamanho; i++) {
             texto += i;
+        }
+    }
+
+    private static void testeStringBuilder(int tamanho) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+        }
+    }
+
+    private static void testeStringBuffer(int tamanho) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
         }
     }
 }
