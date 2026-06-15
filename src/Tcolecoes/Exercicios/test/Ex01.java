3ca01dcbf73647af45f202fe212537e17103a8c4
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-06-15 14:25:29 -0300

    ArrayList

---
+++ src/Tcolecoes/Exercicios/test/Ex01.java
@@ -0,0 +1,21 @@
+package Tcolecoes.Exercicios.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex01 {
+    public static void main() {
+        List<String> cores = new ArrayList<>();
+
+        cores.add("Vermelho");
+        cores.add("Azul");
+        cores.add("Verde");
+        cores.add("Amarelo");
+        cores.add("Rosa");
+
+        for (String cor: cores){
+            System.out.println(cor);
+        }
+
+    }
+}
